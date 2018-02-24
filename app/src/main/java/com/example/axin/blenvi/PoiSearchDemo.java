bedb8bad6438e077639da7a05857fa8739f030ee
15005935651 <804052545@qq.com>
2018-02-25 01:14:52 +0800

    add select point where you  wantto go.

---
+++ app/src/main/java/com/example/axin/blenvi/PoiSearchDemo.java
@@ -0,0 +1,189 @@
+package com.example.axin.blenvi;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.SupportMapFragment;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeOption;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearchOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 演示poi搜索功能
+ */
+public class PoiSearchDemo extends FragmentActivity implements OnGetGeoCoderResultListener,
+        OnGetSuggestionResultListener {
+
+    private SuggestionSearch mSuggestionSearch = null;
+    private BaiduMap mBaiduMap = null;
+    private List<String> suggest;
+    /**
+     * 搜索关键字输入窗口
+     */
+    private AutoCompleteTextView keyWorldsView = null;
+    private ArrayAdapter<String> sugAdapter = null;
+
+    //目的地经纬度
+    public static double endlongitude;
+    public static double endlatitude;
+    public static LatLng endlatLng;
+
+    GeoCoder mSearch = null; // 搜索模块，也可去掉地图模块独立使用
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poisearch);
+
+
+        // 初始化建议搜索模块，注册建议搜索事件监听
+        mSuggestionSearch = SuggestionSearch.newInstance();
+        mSuggestionSearch.setOnGetSuggestionResultListener(this);
+        keyWorldsView = (AutoCompleteTextView) findViewById(R.id.searchkey);
+        sugAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line);
+        keyWorldsView.setAdapter(sugAdapter);
+        keyWorldsView.setThreshold(1);
+        mBaiduMap = ((SupportMapFragment) (getSupportFragmentManager()
+                .findFragmentById(R.id.map))).getBaiduMap();
+        LatLng center = new LatLng(MainActivity.latitude, MainActivity.longitude);
+        MapStatusUpdate status = MapStatusUpdateFactory.newLatLngZoom(center, 19.5f);
+        mBaiduMap.animateMapStatus(status);// 动画的方式到中间
+
+        /**
+         * 当输入关键字变化时，动态更新建议列表
+         */
+        keyWorldsView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1,
+                                          int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2,
+                                      int arg3) {
+                if (cs.length() <= 0) {
+                    return;
+                }
+
+                /**
+                 * 使用建议搜索服务获取建议列表，结果在onSuggestionResult()中更新
+                 */
+                mSuggestionSearch
+                        .requestSuggestion((new SuggestionSearchOption())
+                                .keyword(cs.toString()).city("福州"));
+            }
+        });
+
+        mSearch = GeoCoder.newInstance();
+        mSearch.setOnGetGeoCodeResultListener(this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //  mPoiSearch.destroy();
+        mSuggestionSearch.destroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onGetSuggestionResult(SuggestionResult res) {
+        if (res == null || res.getAllSuggestions() == null) {
+            return;
+        }
+        suggest = new ArrayList<String>();
+        for (SuggestionResult.SuggestionInfo info : res.getAllSuggestions()) {
+            if (info.key != null) {
+                suggest.add(info.key);
+            }
+        }
+        sugAdapter = new ArrayAdapter<String>(PoiSearchDemo.this, android.R.layout.simple_dropdown_item_1line, suggest);
+        keyWorldsView.setAdapter(sugAdapter);
+        sugAdapter.notifyDataSetChanged();
+        mSearch.geocode(new GeoCodeOption().city("福建").address( keyWorldsView.getText().toString()));
+    }
+
+    @Override
+    public void onGetGeoCodeResult(GeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+//            Toast.makeText(PoiSearchDemo.this, "抱歉，未能找到结果", Toast.LENGTH_LONG)
+//                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.icon_marka)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        String strInfo = String.format("纬度：%f 经度：%f",
+                result.getLocation().latitude, result.getLocation().longitude);
+    }
+
+    @Override
+    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+//            Toast.makeText(PoiSearchDemo.this, "抱歉，未能找到结果", Toast.LENGTH_LONG)
+//                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.icon_gcoding)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        endlatLng=new LatLng(result.getLocation().latitude,result.getLocation().longitude);
+
+    }
+
+
+}
